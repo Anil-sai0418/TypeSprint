5882caaf42d80c61366e6c88e35de6cd3c47ebf7
Nunna anil sai <anilsainunna@gmail.com>
2026-04-08 20:26:46 +0530

    fix:fixed the production email breaking issue.

--- backend/server/utils/emailService.js
+++ backend/server/utils/emailService.js
@@ -7,7 +7,9 @@ const createTransporter = () => {
     }
     
     return nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
